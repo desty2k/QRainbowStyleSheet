da9f43d98754cb79a08bc450d8807b7030432149
desty2k <55806660+desty2k@users.noreply.github.com>
2020-11-24 10:33:15 +0100

    Revert "rename project in setup file"
    This reverts commit 88811412da080ea11b32ea432ad64497ae5abea6.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def remove_all(dir_path, patterns='*.pyc'):
 
 
 setup(
-    name='QRainbowStyle',
+    name='QDarkStyle',
     version=__version__,
     packages=find_packages(),
     url='https://github.com/ColinDuquesnoy/QDarkStyleSheet',
