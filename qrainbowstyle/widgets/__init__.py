04382c3eec93b501769fedcccd394cae52039deb
desty2k <55806660+desty2k@users.noreply.github.com>
2020-12-06 19:17:08 +0100

    UPD: widgets imports

--- qrainbowstyle/widgets/__init__.py
+++ qrainbowstyle/widgets/__init__.py
@@ -1,1 +1,3 @@
 from .QtWaitingSpinner.pyqtspinner.spinner import WaitingSpinner
+from .GoogleMapsView.MapsWidget import GoogleMapsView
+from .PythonQtWidgets.qtwidgets.palette.palette import StylePicker
