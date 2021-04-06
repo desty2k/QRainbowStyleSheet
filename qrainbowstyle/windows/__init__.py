9e1ebfe7e64b6bf314bb430cf8d1c37fa074efd3
desty2k <55806660+desty2k@users.noreply.github.com>
2021-04-06 18:26:30 +0200

    UPD: remove info about parent windows

--- qrainbowstyle/windows/__init__.py
+++ qrainbowstyle/windows/__init__.py
@@ -5,8 +5,7 @@ import sys
 if sys.platform == "win32" and not os.getenv("FORCE_NON_WIN"):
     from .FramelessWindowWin import FramelessWindow
     import logging
-    logging.getLogger().info("Imported frameless windows with Windows API support. "
-                             "Setting parent for windows is not supported.")
+    logging.getLogger().info("Imported frameless windows with Windows API support")
 else:
     from .FramelessWindow import FramelessWindow
 
