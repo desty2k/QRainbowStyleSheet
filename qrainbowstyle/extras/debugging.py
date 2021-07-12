dd1ea62fcaf12048ca12e56528576f7c704aca1f
desty2k <wojciech.wentland@int.pl>
2021-07-12 19:03:27 +0200

    UPD: try to import coloredlogs when creating logger

--- qrainbowstyle/extras/debugging.py
+++ qrainbowstyle/extras/debugging.py
@@ -5,20 +5,43 @@ from qtpy.QtCore import QtInfoMsg, QtFatalMsg, QtCriticalMsg, QtWarningMsg
 
 
 class OutputLogger:
-    """Logging cocfiguration class"""
-
     def __init__(self):
         super(OutputLogger, self).__init__()
-        logging.basicConfig(
-            level=logging.NOTSET,
-            format="%(asctime)s [%(threadName)s] [%(name)s] [%(levelname)s] %(message)s",
-            handlers=[logging.StreamHandler()])
-        logging.getLogger(self.__class__.__name__).debug("Logger enabled.")
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.NOTSET)
+
+        self.handler = logging.StreamHandler()
+        self.handler.setLevel(logging.NOTSET)
+
+        try:
+            import coloredlogs
+            self.formatter = coloredlogs.ColoredFormatter("%(asctime)s "
+                                                          "[%(threadName)s] "
+                                                          "[%(name)s] "
+                                                          "[%(levelname)s] "
+                                                          "%(message)s")
+        except ImportError:
+            self.formatter = logging.Formatter("%(asctime)s "
+                                               "[%(threadName)s] "
+                                               "[%(name)s] "
+                                               "[%(levelname)s] "
+                                               "%(message)s")
+
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+        self.logger.info("Logger enabled")
+
+    def set_level(self, level):
+        if self.logger and self.handler:
+            self.logger.setLevel(level)
+            self.handler.setLevel(level)
+        else:
+            raise Exception("Logger not enabled!")
 
 
 def qt_message_handler(mode, context, message):
+    """Logger for Qt errors"""
     logger = logging.getLogger("QT Logger")
-    """Qt errors handler"""
     if mode == QtInfoMsg:
         mode = 20
     elif mode == QtWarningMsg:
